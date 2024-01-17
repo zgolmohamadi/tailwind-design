73f24747be034695d9e3bfb0ed353b4ea84920a8
zahrag@evenset.com <35527928+zgolmohamadi@users.noreply.github.com>
2024-01-17 18:48:26 +0330

    add home page

--- home/scripts.js
+++ home/scripts.js
@@ -1,11 +1,58 @@
 $(document).ready(function () {
   $('.main-slider').slick({
-    dots: true,
-    infinite: true,
+    infinite: false,
     speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     variableWidth: true,
-    
+    centerMode: false,
+    rtl: true,
+    centerPadding: '14.8125rem',
+    arrows: true,
+    prevArrow:
+      "<button class='a-left control-c prev slick-prev'><img  src='/assets/img/arrow-right.svg'></button>",
+    nextArrow:
+      "<button class='a-right control-c next slick-next'><img  src='/assets/img/arrow-left.svg'></button>",
   });
+
+    //product carasual
+
+    $('.carousel').slick({
+      slidesToShow: 6,
+      slidesToScroll: 3,
+      infinite: false,
+      rtl: true,
+      arrows: true,
+      prevArrow:
+        "<button class='a-left control-c prev slick-prev'><img  src='/assets/img/arrow-right.svg'></button>",
+      nextArrow:
+        "<button class='a-right control-c next slick-next'><img  src='/assets/img/arrow-left.svg'></button>",
+      responsive: [
+        {
+          breakpoint: 1280,
+          settings: {
+            rtl: true,
+            arrows: false,
+            slidesToShow: 4,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            rtl: true,
+            arrows: false,
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 640,
+          settings: {
+            rtl: true,
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
 });
